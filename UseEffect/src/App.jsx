bc19ee3f4b3576f9c15ddae997078a7c275988ad
Christian Sestito <sestitochristian4@gmail.com>
2025-06-21 17:11:29 +0200

    UseEffect

---
+++ UseEffect/src/App.jsx
@@ -0,0 +1,14 @@
+import Counter from './Counter';
+import Clock from './Clock';
+
+function App() {
+  return (
+    <div>
+      <h1>React Counter and Clock</h1>
+      <Counter/>
+      <Clock />
+    </div>
+  );
+}
+
+export default App;
