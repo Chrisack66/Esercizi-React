885610d0b4fda62ce1a6e7100b1bb25cb0090550
Christian Sestito <sestitochristian4@gmail.com>
2025-06-24 15:46:21 +0200

    Handling_Events

--- Esercizi_React/src/App.jsx
+++ Esercizi_React/src/App.jsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
-import AlertClock from './AlertClock'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
+import MouseClicker from './MouseClicker'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-  <AlertClock />
+      <MouseClicker />
     </>
   )
 }
