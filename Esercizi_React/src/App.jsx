90484a0c6306eaf6bd896f439a55b1178ecffd8d
Christian Sestito <sestitochristian4@gmail.com>
2025-06-28 10:56:56 +0200

    Lists

--- Esercizi_React/src/App.jsx
+++ Esercizi_React/src/App.jsx
@@ -1,14 +1,16 @@
-import React from 'react';
-import FocusableInput from './FocusableInput';
-import EffectOnMount from './EffectOnMount';
+import Colors from './Colors';
 
 function App() {
+  const colorList = [
+    { id: 1, name: 'Red' },
+    { id: 2, name: 'Green' },
+    { id: 3, name: 'Blue' },
+  ];
+
   return (
     <div>
-      <h1>Focusable Input</h1>
-      <FocusableInput />
-
-      <EffectOnMount />
+      <h1>Lista colori</h1>
+      <Colors colors={colorList} />
     </div>
   );
 }
